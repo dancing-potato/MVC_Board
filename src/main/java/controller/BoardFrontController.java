646c0fa0d14e346ec5bc4657ad196b6f8306ff9c
zero kim <ziroground@naver.com>
2022-09-22 23:16:22 +0900

    .

---
+++ src/main/java/controller/BoardFrontController.java
@@ -0,0 +1,37 @@
+package controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("*.bo")
+public class BoardFrontController extends HttpServlet {
+	
+	// GET 방식 & POST 방식 요청에 대한 공통 작업을 수행하는 doProcess() 메서드 정의
+	protected void doProcess(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+//		System.out.println("BoardFrontController");
+		
+		// POST 방식에 대한 한글 처리
+		request.setCharacterEncoding("UTF-8");
+		
+		// 서블릿 주소 추출
+		String command = request.getServletPath();
+		System.out.println("command : " + command);
+		
+		
+		
+	}
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doProcess(request, response);
+	}
+
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doProcess(request, response);
+	}
+
+}
